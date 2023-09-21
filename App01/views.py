d0cca721370c21b9a0d8f9fd467ee2306f7ef47f
Critobal Navarrete <cristobalnavarrete2019@gmail.com>
2023-09-21 08:57:48 -0300

    App 01 .2

--- App01/views.py
+++ App01/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def vista1(request):
-      return  HttpResponse("<h1> esta es la primera vista de la Rama App 01 </h1>")
+      return  HttpResponse("<h1 style> esta es la primera vista de la Rama App 01 </h1>"
+                           "<h2 style> esta es la primera vista de la Rama App 01 </h2>"
+                           "<h3 style> esta es la primera vista de la Rama App 01 </h3>"
+                           "<h4 style> esta es la primera vista de la Rama App 01 </h4>"
+                           "<ul>"
+                            "<li>1</li>"
+                            "<li>2</li>"
+                            "<li>3</li>"
+                            "</ul>")
 def vista2(request):
-      return  HttpResponse("<h1> esta es la segunda vista de la Rama App 01 </h1>")
+            return  HttpResponse("<h1 style> esta es la Segunda vista de la Rama App 01 </h1>"
+                           "<h2 style> esta es la Segunda vista de la Rama App 01 </h2>"
+                           "<h3 style> esta es la Segunda vista de la Rama App 01 </h3>"
+                           "<h4 style> esta es la Segunda vista de la Rama App 01 </h4>"
+                           "<ul>"
+                           "<li>4</li>"
+                            "<li>5</li>"
+                            "<li>6</li>"
+                            "</ul>")
